c33a7681c5c9b16ba31d8fb8fb7bff11d8001422
Ayushkumar0oo0 <ayush9123288616@gmail.com>
2025-05-12 23:12:37 +0530

    Update MyConnection.java

--- src/main/java/db/MyConnection.java
+++ src/main/java/db/MyConnection.java
@@ -12,7 +12,21 @@ public class MyConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/student", "SqL name", "SQl passsword"
-            );
+            ); // create database 
+//             CREATE TABLE users (
+//     user_id INT PRIMARY KEY AUTO_INCREMENT,
+//     username VARCHAR(50) NOT NULL UNIQUE,
+//     email VARCHAR(100) NOT NULL UNIQUE,
+//     password VARCHAR(255) NOT NULL
+// );
+// CREATE TABLE files (
+//     file_id INT PRIMARY KEY AUTO_INCREMENT,
+//     user_id INT NOT NULL,
+//     file_path TEXT NOT NULL,
+//     is_hidden BOOLEAN DEFAULT FALSE,
+//     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+// );
+
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
